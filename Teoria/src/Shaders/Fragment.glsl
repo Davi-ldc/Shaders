96416f79bb088f49c98628e292c09a9719909701
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2023-09-06 20:59:56 -0300

    mais teoria

--- Teoria/src/Shaders/Fragment.glsl
+++ Teoria/src/Shaders/Fragment.glsl
@@ -1,6 +1,12 @@
 precision mediump float;
 
+
+varying float fRandom;
+varying float Time2;
+
+
 void main()
 {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    float t = sin(Time2);
+    gl_FragColor = vec4(smoothstep(0.0, 1.0, t), 0.0, 1.0 - smoothstep(0.0, 1.0, t), 1.0);//rgb e alpha
 }
